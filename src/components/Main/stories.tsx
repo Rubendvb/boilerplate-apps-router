6aacba954f95e6d7d7087c20694cdac769a73d50
Rubendvb <rubenvasquez556@gmail.com>
2024-07-21 19:37:53 -0300

    add CI workflow

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -5,9 +5,20 @@ import Main from '.'
 export default {
   title: 'Main',
   component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'TypeScript, ReactJS, NextJS e Styled Components'
+  },
   parameters: {
     layout: 'fullscreen'
   }
 } as Meta
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'Title Basic',
+    description: 'Description Basic'
+  }
+}
